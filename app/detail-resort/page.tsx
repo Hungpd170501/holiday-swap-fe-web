7c732a9987b8b5e920cf0c6703ebe9298bb09fb4
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-09-14 10:07:43 +0700

    Login_DetailResort_UI

---
+++ app/detail-resort/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import HeaderDetailResort from "../components/detailResort/HeaderDetailResort";
+import Header from "../components/Header";
+import DetailContentTop from "../components/detailResort/DetailContentTop";
+import ImageDetailResort from "../components/detailResort/ImageDetailResort";
+import DetailContent from "../components/detailResort/DetailContent";
+import Footer from "../components/Footer";
+import SimpleMap from "../components/detailResort/Map";
+import RightSideBar from "../components/detailResort/RightSideBar";
+
+export default function DetailResort() {
+  return (
+    <div className="flex flex-col ">
+      <Header />
+      <div className="px-[70px]">
+        <HeaderDetailResort />
+      </div>
+      <div className="w-full h-[0.5px] bg-gray-300 mt-[0px]"></div>
+      <div className="px-[70px]">
+        <DetailContentTop />
+      </div>
+      <div className="px-[70px] pb-[20px]">
+        <ImageDetailResort />
+      </div>
+      <div className="flex flex-row px-[70px]  ">
+        <DetailContent />
+        <RightSideBar />
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
