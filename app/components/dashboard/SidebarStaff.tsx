280bcd9aff36dcaa571fe6f55f282855c7f101fc
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-09-28 10:54:42 +0700

    Staff UI

---
+++ app/components/dashboard/SidebarStaff.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import React, { useState } from "react";
+import { GrPersonalComputer, GrSecure } from "react-icons/gr";
+import { FiEdit } from "react-icons/fi";
+import { PiNotepadBold } from "react-icons/pi";
+import { LiaFileInvoiceDollarSolid } from "react-icons/lia";
+import { AiOutlineStar } from "react-icons/ai";
+import { MdOutlineSwapHorizontalCircle } from "react-icons/md";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const SidebarStaff = () => {
+  const pathName = usePathname();
+  const sidebarMyaccount = [
+    {
+      name: "Dashboard",
+      href: "/staff",
+      icon: GrPersonalComputer,
+      current: pathName === "/staff" ? true : false,
+    },
+    {
+      name: "List Resort",
+      href: "/staff/listresort",
+      icon: FiEdit,
+      current: pathName === "/staff/listresort" ? true : false,
+    },
+    {
+      name: "Create Resort",
+      href: "/staff/createresort",
+      icon: GrSecure,
+      current: pathName === "/staff/createresort" ? true : false,
+    },
+  ];
+  const sidebarBooking = [
+    {
+      name: "List Membership",
+      href: "/staff/listmember",
+      icon: PiNotepadBold,
+      current: pathName === "/staff/listmember" ? true : false,
+    },
+    {
+      name: "Banned",
+      href: "/staff/banned",
+      icon: LiaFileInvoiceDollarSolid,
+      current: pathName === "/staff/banned" ? true : false,
+    },
+  ];
+  const sidebarExchange = [
+    {
+      name: "List Approve",
+      href: "/staff/listapprove",
+      icon: MdOutlineSwapHorizontalCircle,
+      current: pathName === "/staff/listapprove" ? true : false,
+    },
+    {
+      name: "Infomation",
+      href: "/staff/infomation",
+      icon: LiaFileInvoiceDollarSolid,
+      current: pathName === "/staff/infomation" ? true : false,
+    },
+  ];
+  return (
+    <div className="pt-[20px] pl-5 pr-5">
+      <div className="hidden lg:flex lg:min-h-full lg:rounded-md lg:w-72 lg:flex-col h-full">
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-[#F8F8F8] px-6 pb-4 border-r-2">
+          <div className="flex h-16 shrink-0 items-center">
+            <h1 className="text-3xl font-bold text-gray-700">Resort</h1>
+          </div>
+
+          <div className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {sidebarMyaccount.map((option) => (
+                    <li key={option.name}>
+                      <Link
+                        href={option.href}
+                        className={classNames(
+                          option.current
+                            ? "bg-common text-white"
+                            : "text-gray-400 hover:text-white hover:bg-common",
+                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        )}
+                      >
+                        <option.icon className="text-gray-300 group-hover:text-white h-6 w-6 shrink-0" />
+                        {option.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="flex h-16 shrink-0 items-center">
+            <h1 className="text-3xl font-bold text-gray-700">Member</h1>
+          </div>
+          <div className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {sidebarBooking.map((option) => (
+                    <li key={option.name}>
+                      <Link
+                        href={option.href}
+                        className={classNames(
+                          option.current
+                            ? "bg-common text-white"
+                            : "text-gray-400 hover:text-white hover:bg-common",
+                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        )}
+                      >
+                        <option.icon className="text-gray-300 group-hover:text-white h-6 w-6 shrink-0" />
+                        {option.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="flex h-16 shrink-0 items-center">
+            <h1 className="text-3xl font-bold text-gray-700">Approve</h1>
+          </div>
+          <div className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {sidebarExchange.map((option) => (
+                    <li key={option.name}>
+                      <Link
+                        href={option.href}
+                        className={classNames(
+                          option.current
+                            ? "bg-common text-white"
+                            : "text-gray-400 hover:text-white hover:bg-common",
+                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                        )}
+                      >
+                        <option.icon className="text-gray-300 group-hover:text-white h-6 w-6 shrink-0" />
+                        {option.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button className="bg-[#5C98F2] px-4 py-3 rounded-md text-white">
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarStaff;
---
+++ app/staff/layout.tsx
@@ -0,0 +1,20 @@
+import Provider from "../components/Provider";
+import React from "react";
+import SidebarStaff from "../components/dashboard/SidebarStaff";
+
+export default async function DashboardStaffLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Provider>
+      <div className="flex flex-row gap-10 pt-20">
+        <div>
+          <SidebarStaff />
+        </div>
+        <main className="w-full pr-14">{children}</main>
+      </div>
+    </Provider>
+  );
+}
