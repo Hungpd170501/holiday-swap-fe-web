33e1e029cfca518aa94b0e1d2a8c42023ff2c3ef
Tin Nguyen <tinitgithub@gmail.com>
2023-10-27 20:14:21 +0700

    Fix type

--- app/components/sidebar-search-resort/RatingPicker.tsx
+++ app/components/sidebar-search-resort/RatingPicker.tsx
@@ -5,7 +5,7 @@ import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 export default function RatingPicker() {
   const [rating, setRating] = useState(0);
 
-  const handleStarClick = (index) => {
+  const handleStarClick = (index: number) => {
     setRating(index + 1);
   };
 
