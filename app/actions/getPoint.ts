d3e3e39a2659101eb7fb40a7f9604536c75c98d4
Tin Nguyen <tinitgithub@gmail.com>
2023-10-27 19:02:36 +0700

    Fix axios in getPoint function

--- app/actions/getPoint.ts
+++ app/actions/getPoint.ts
@@ -1,9 +1,14 @@
+import { getServerSession } from "next-auth";
 import useAxiosAuth from "../hooks/useAxiosAuth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import axios from "axios";
 
 export default async function GetPoint() {
   try {
-    const axiosAuth = useAxiosAuth();
-    const point = await (await axiosAuth).get("/point");
+    const session = await getServerSession(authOptions);
+    const accessToken = session?.user?.access_token;
+    const config = { headers: { Authorization: `Bearer ${accessToken}` } };
+    const point = await axios.get(`${process.env.API_URL}/point`);
 
     console.log("Check point", point.data);
 
