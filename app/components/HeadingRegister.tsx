a33536a45acf3546acbc19608ec11c3cda4b78fe
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-11-04 15:12:41 +0700

    Create apartment in register account

--- app/components/HeadingRegister.tsx
+++ app/components/HeadingRegister.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
 interface HeadingRegsiterProps {
   label: string;
@@ -8,17 +8,11 @@ interface HeadingRegsiterProps {
   className?: string;
 }
 
-const HeadingRegister: React.FC<HeadingRegsiterProps> = ({
-  label,
-  width,
-  className,
-}) => {
+const HeadingRegister: React.FC<HeadingRegsiterProps> = ({ label, width, className }) => {
   return (
     <Fragment>
-      <div
-        className={`bg-[#F5F5F7] flex items-center justify-center ${className}`}
-      >
-        <div className="text-xl md:text-5xl pt-[150px] px-5 text-center md:pt-[250px] pb-[70px]">
+      <div className={`bg-[#F5F5F7] flex items-center justify-center ${className}`}>
+        <div className="text-xl md:text-5xl pt-[150px] px-5 text-center md:pt-[150px] pb-[80px]">
           {label}
         </div>
       </div>
