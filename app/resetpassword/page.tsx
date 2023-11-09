58de42cfded5c0ae6482c754a7ff70801f94b969
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-11-09 22:37:02 +0700

    Reset password

---
+++ app/resetpassword/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ResetPasswordComponent from '../components/resetpassword/ResetPasswordComponent';
+
+export default function ResetPassword() {
+  return (
+    <div>
+      <ResetPasswordComponent />
+    </div>
+  );
+}
