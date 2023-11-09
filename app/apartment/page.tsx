7c2f40a595732ffd85caeddfa1af2d241342ad7f
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-11-09 14:43:24 +0700

    Filter apartment

--- app/apartment/page.tsx
+++ app/apartment/page.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import ClientOnly from "../components/ClientOnly";
-import ListAparment from "./ListAparment";
-import GetListApartment from "../actions/getListApartment";
-import GetListResort from "../actions/getListResort";
+import React from 'react';
+import ClientOnly from '../components/ClientOnly';
+import ListAparment from './ListAparment';
+import GetListApartment from '../actions/getListApartment';
+import GetListResort from '../actions/getListResort';
+import { AiFillAccountBook } from 'react-icons/ai';
 
 export const metadata = {
-  title: "Apartments",
+  title: 'Apartments',
 };
 
 export default async function listResortPage() {
   const listApartment = await GetListApartment();
-  const listResort = await GetListResort("0");
+  const listResort = await GetListResort('0');
   return (
     <ClientOnly>
       <ListAparment listApartment={listApartment} listResort={listResort} />
