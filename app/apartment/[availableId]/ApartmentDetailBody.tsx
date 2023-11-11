08e8b55da2233f5ef533fbaa6db2cdf014d5597b
ptKien <phamthanhkien2k1@gmail.com>
2023-11-12 01:19:05 +0700

    ui

--- app/apartment/[availableId]/ApartmentDetailBody.tsx
+++ app/apartment/[availableId]/ApartmentDetailBody.tsx
@@ -8,7 +8,8 @@ import { addDays, addMonths, differenceInDays, format, isAfter, isBefore, subDay
 import GoogleMapReact from 'google-map-react-concurrent';
 import { useDateRange } from '../DateRangeContext';
 import CalendarAparmentBody from '../CalendarAparmentBody';
-
+import { Avatar, Rate, Typography } from 'antd';
+const { Text } = Typography;
 interface ApartmentDetailBodyProps {
   apartment?: any;
   dateRange: any;
@@ -211,6 +212,114 @@ const ApartmentDetailBody: React.FC<ApartmentDetailBodyProps> = ({
               text={apartment.availableTime.pricePerNight}
             />
           </GoogleMapReact>
+        </div>
+      </div>
+      <div className="flex flex-col py-8 border-b border-gray-500">
+        <div className="py-8">
+          <div className="text-2xl font-bold">Reviews</div>
+          {/* rate point */}
+          <div className="flex items-center justify-items-stretch">
+            <div>
+              <span className="text-5xl text-orange-500">4</span>
+              <span className="text-base">/5</span>
+            </div>
+            <Typography className="px-8">Tuyệt vời</Typography>
+            <Typography className="p">21 Bình luận</Typography>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 content-evenly">
+          <div>
+            <div className="p-3 rounded border-2 border-gray-300">
+              <div className="flex items-center justify-items-stretch border-b pb-1">
+                <Avatar size="large">U</Avatar>
+                <div className="pl-2">
+                  <Text className="text-base">Kien</Text>
+                  <br />
+                  <Text>Sainte-Hélène, France</Text>
+                </div>
+              </div>
+              <div className="py-1">
+                <Rate allowHalf defaultValue={2.5} disabled />
+              </div>
+              <div>
+                <Text>
+                  Warm welcome, I recommend, easy to walk to restaurants and barsWarm welcome, I
+                  recommend, easy to wa
+                </Text>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="p-3 rounded border-2 border-gray-300">
+              <div className="flex items-center justify-items-stretch border-b pb-1">
+                <Avatar size="large">U</Avatar>
+                <div className="pl-2">
+                  <Text className="text-base">Kien</Text>
+                  <br />
+                  <Text>Sainte-Hélène, France</Text>
+                </div>
+              </div>
+              <div className="py-1">
+                <Rate allowHalf defaultValue={2.5} disabled />
+              </div>
+              <div>
+                <Text>
+                  Warm welcome, I recommend, easy to walk to restaurants and barsWarm welcome, I
+                  recommend, easy to walk to restaurants and barsWarm welcome, I recommend, easy to
+                  walk to restaurants and bars Warm welcome, I recommend, easy to walk to
+                  restaurants and barsWarm welcome, I recommend, easy to walk to restaurants and
+                  barsWarm welcome, I recommend, easy to walk to restaurants and bars
+                </Text>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="p-3 rounded border-2 border-gray-300">
+              <div className="flex items-center justify-items-stretch border-b pb-1">
+                <Avatar size="large">U</Avatar>
+                <div className="pl-2">
+                  <Text className="text-base">Kien</Text>
+                  <br />
+                  <Text>Sainte-Hélène, France</Text>
+                </div>
+              </div>
+              <div className="py-1">
+                <Rate allowHalf defaultValue={2.5} disabled />
+              </div>
+              <div>
+                <Text>
+                  Warm welcome, I recommend, easy to walk to restaurants and barsWarm welcome, I
+                  recommend, easy to walk to restaurants and barsWarm welcome, I recommend, easy to
+                  walk to restaurants and bars
+                </Text>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="p-3 rounded border-2 border-gray-300">
+              <div className="flex items-center justify-items-stretch border-b pb-1">
+                <Avatar size="large">U</Avatar>
+                <div className="pl-2">
+                  <Text className="text-base">Kien</Text>
+                  <br />
+                  <Text>Sainte-Hélène, France</Text>
+                </div>
+              </div>
+              <div className="py-1">
+                <Rate allowHalf defaultValue={2.5} disabled />
+              </div>
+              <div>
+                <Text>
+                  Warm welcome, I recommend, easy to walk to restaurants and barsWarm welcome, I
+                  recommend, easy to walk to restaurants and barsWarm welcome, I recommend, easy to
+                  walk to restaurants and bars Warm welcome, I recommend, easy to walk to
+                  restaurants and barsWarm welcome, I recommend, easy to walk to restaurants and
+                  barsWarm welcome, I recommend, easy to walk to restaurants and bars Warm welcome,
+                </Text>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
