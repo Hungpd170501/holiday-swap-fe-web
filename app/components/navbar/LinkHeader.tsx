c8273c441c9ea3f9359b37a892dfbf4493ade86c
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-11-14 23:16:35 +0700

    Detail Blog

--- app/components/navbar/LinkHeader.tsx
+++ app/components/navbar/LinkHeader.tsx
@@ -13,9 +13,9 @@ const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
     children: React.ReactElement;
   },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
-  return <Slide direction='up' ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const LinkHeader = () => {
@@ -31,9 +31,8 @@ const LinkHeader = () => {
     setOpen(false);
   };
 
-
   return (
-    <div className='flex flex-row space-x-9 text-gray-500'>
+    <div className="flex flex-row space-x-9 text-gray-500">
       <div
         onClick={() => router.push('/')}
         className={`hover:text-black cursor-pointer flex flex-col items-center ${
@@ -42,7 +41,7 @@ const LinkHeader = () => {
       >
         Home
         {pathname === '/' ? (
-          <span className='bg-gray-300 rounded-full w-2 h-2'></span>
+          <span className="bg-gray-300 rounded-full w-2 h-2"></span>
         ) : (
           <span></span>
         )}
@@ -55,34 +54,46 @@ const LinkHeader = () => {
       >
         Aparment
         {pathname === '/apartment' ? (
-          <span className='bg-gray-300 rounded-full w-2 h-2'></span>
+          <span className="bg-gray-300 rounded-full w-2 h-2"></span>
         ) : (
           <span></span>
         )}
       </div>
       <div
-        onClick={handleClickOpen}
-        className='hover:text-black cursor-pointer list-none'
+        onClick={() => router.push('/blog')}
+        className={`hover:text-black cursor-pointer flex flex-col items-center ${
+          pathname === '/blog' ? 'text-black' : ''
+        }`}
       >
-        <li className='rounded-full cursor-pointer'>
-          <p className='flex font-medium items-center'>
-            <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 mr-2' viewBox='0 0 20 20' fill='currentColor'>
-              <path fillRule='evenodd'
-                    d='M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z'
-                    clipRule='evenodd' />
+        Blogs
+        {pathname === '/blog' ? (
+          <span className="bg-gray-300 rounded-full w-2 h-2"></span>
+        ) : (
+          <span></span>
+        )}
+      </div>
+      <div onClick={handleClickOpen} className="hover:text-black cursor-pointer list-none">
+        <li className="rounded-full cursor-pointer">
+          <p className="flex font-medium items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 mr-2"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z"
+                clipRule="evenodd"
+              />
             </svg>
             Map View
           </p>
         </li>
       </div>
-      <Dialog
-        fullScreen
-        open={open}
-        onClose={handleClose}
-        TransitionComponent={Transition}
-      >
+      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
         <button
-          className='bg-black text-white py-2 px-8 rounded my-2 hover:bg-gray-600 transition ease-in duration-100 fixed right-16 bottom-3.5 w-fit z-10'
+          className="bg-black text-white py-2 px-8 rounded my-2 hover:bg-gray-600 transition ease-in duration-100 fixed right-16 bottom-3.5 w-fit z-10"
           // OnClick - Router Routes Back to the last page
           onClick={handleClose}
         >
@@ -90,7 +101,6 @@ const LinkHeader = () => {
         </button>
         <MainMap data={undefined}></MainMap>
       </Dialog>
-
     </div>
   );
 };
--- app/staff/layout.tsx
+++ app/staff/layout.tsx
@@ -6,10 +6,10 @@ export default async function DashboardStaffLayout({ children }: { children: Rea
   return (
     <Provider>
       <div className="flex flex-row gap-10 pt-20">
-        <div>
+        <div className="sticky col-span-4 top-0 h-full">
           <SidebarStaff />
         </div>
-        <main className="w-full pr-14">{children}</main>
+        <main className="w-full  h-full pr-14 col-span-8">{children}</main>
       </div>
     </Provider>
   );
