686f37d890f45fbace22581d701b8648e4bbd6be
Tin Nguyen <tinitgithub@gmail.com>
2023-11-27 15:19:49 +0700

    Fix bug

--- app/actions/getListApartment.ts
+++ app/actions/getListApartment.ts
@@ -7,15 +7,28 @@ export default async function GetListApartment() {
     const session = await getServerSession(authOptions);
     const accessToken = session?.user?.access_token;
     const config = { headers: { Authorization: `Bearer ${accessToken}` } };
-    const listApartment = await axios.get(
-      `${process.env.API_URL}/apartment-for-rent?guest=1&numberBedsRoom=1&numberBathRoom=1&pageNo=0&pageSize=9&sortBy=id&sortDirection=asc`,
-      config
-    );
 
-    if (!listApartment) {
-      return null;
+    if (session && accessToken) {
+      const listApartment = await axios.get(
+        `${process.env.API_URL}/apartment-for-rent?guest=1&numberBedsRoom=1&numberBathRoom=1&pageNo=0&pageSize=9&sortBy=id&sortDirection=asc`,
+        config
+      );
+
+      if (!listApartment) {
+        return null;
+      }
+
+      return listApartment.data;
+    } else {
+      const listApartment = await axios.get(
+        `${process.env.API_URL}/apartment-for-rent?guest=1&numberBedsRoom=1&numberBathRoom=1&pageNo=0&pageSize=9&sortBy=id&sortDirection=asc`
+      );
+
+      if (!listApartment) {
+        return null;
+      }
+
+      return listApartment.data;
     }
-
-    return listApartment.data;
   } catch (error) {}
 }
