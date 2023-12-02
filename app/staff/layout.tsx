6a68d9f4d419eb6da203291a58e874a642bb9e88
Tin Nguyen <tinitgithub@gmail.com>
2023-12-02 15:28:56 +0700

    Remove provider session from layout staff

--- app/staff/layout.tsx
+++ app/staff/layout.tsx
@@ -4,13 +4,11 @@ import SidebarStaff from '../components/dashboard/SidebarStaff';
 
 export default async function DashboardStaffLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Provider>
-      <div className="flex flex-row gap-10 pt-20">
-        <div className="sticky top-20 col-span-4 h-full">
-          <SidebarStaff />
-        </div>
-        <main className="w-full mt-8 h-full pr-14 col-span-8">{children}</main>
+    <div className="flex flex-row gap-10 pt-20">
+      <div className="sticky top-20 col-span-4 h-full">
+        <SidebarStaff />
       </div>
-    </Provider>
+      <main className="w-full mt-8 h-full pr-14 col-span-8">{children}</main>
+    </div>
   );
 }
