b4280a6a0ae60b03ac63809942384099e56db3af
ptKien <phamthanhkien2k1@gmail.com>
2023-12-11 01:18:14 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import HomePage from './HomePage';
 import GetListApartment from './actions/getListApartment';
 import GetListResort from './actions/getListResort';
-import GetListResortForCreateOwner from '@/app/actions/getListResortForCreateOwner';
+import GetListResortForRent from '@/app/actions/getListResortForRent';
 
 export default async function Home() {
-  const listResort = await GetListResortForCreateOwner('0');
-  const apartment = await GetListApartment();
-  return <HomePage listResort={listResort} apartment={apartment} />;
+  return <HomePage  />;
 }
