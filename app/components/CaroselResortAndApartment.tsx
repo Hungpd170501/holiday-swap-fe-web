d40e380683113f0aae1f8624bad4a9bdb0b3d9fd
ptKien <phamthanhkien2k1@gmail.com>
2023-12-16 17:23:52 +0700

    update

--- app/components/CaroselResortAndApartment.tsx
+++ app/components/CaroselResortAndApartment.tsx
@@ -1,18 +1,23 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import React, { Fragment, useEffect, useState } from 'react';
-import { Input, theme } from 'antd';
+import { Avatar, Input, theme } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { MdStar } from 'react-icons/md';
 import getListResortForRent from '../actions/getListResortForRent';
 import { Pagination } from 'flowbite-react';
 import getListApartmentForRent from '../actions/getListApartmentForRent';
+import { Col, Divider, Row, Carousel, Image, Card } from 'antd';
+const { Meta } = Card;
 import {
   CalendarOutlined,
   CaretRightOutlined,
   LeftOutlined,
   RightOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
 import GallerySlider from './map/AnyReactComponent/GallerySlider';
 interface IPageable {
@@ -24,7 +29,7 @@ interface IPageable {
 interface IPageableAparment {
   pageNo: number;
   pageSize: number;
-  totalPages?: number;
+  totalPages: number;
 }
 interface IParams {
   locationName?: string;
@@ -74,31 +79,32 @@ const CaroselResortAndApartment: React.FC = ({}) => {
   const [pageable1, setPageable1] = useState<IPageableAparment>({
     pageNo: 0,
     pageSize: 4,
+    totalPages: 0,
   });
   const [pageable2, setPageable2] = useState<IPageableAparment>({
     pageNo: 0,
     pageSize: 4,
+    totalPages: 0,
   });
   const [pageable3, setPageable3] = useState<IPageableAparment>({
     pageNo: 0,
     pageSize: 4,
+    totalPages: 0,
   });
   const [pageable4, setPageable4] = useState<IPageableAparment>({
     pageNo: 0,
     pageSize: 4,
+    totalPages: 0,
   });
   const [pageable5, setPageable5] = useState<IPageableAparment>({
     pageNo: 0,
     pageSize: 4,
+    totalPages: 0,
   });
 
   const { token } = theme.useToken();
   const [current, setCurrent] = useState(0);
-  const router = useRouter();
-  const handleRedirectApartmentDetail = (url: string) => {
-    // newDateRange.setNew();
-    route.push(url);
-  };
+
   useEffect(() => {
     loadListResortAndApartment();
   }, []);
@@ -166,7 +172,10 @@ const CaroselResortAndApartment: React.FC = ({}) => {
     });
   };
 
-  const showCalendar = () => {};
+  const handleRedirectApartmentDetail = (url: string) => {
+    // newDateRange.setNew();
+    route.push(url);
+  };
 
   return (
     <>
@@ -174,7 +183,7 @@ const CaroselResortAndApartment: React.FC = ({}) => {
         {
           var listApartment: any = [];
           //default pageabble
-          var pageable: IPageableAparment = { pageNo: 0, pageSize: 4 };
+          var pageable: IPageableAparment = { pageNo: 0, pageSize: 4, totalPages: 0 };
           switch (index) {
             case 0:
               listApartment = listApartment1;
@@ -199,153 +208,146 @@ const CaroselResortAndApartment: React.FC = ({}) => {
             default:
               break;
           }
+          console.log(listApartment);
         }
         return (
-          <div key={index}>
-            {/* <div>
-              <div className="w-full flex flex-row justify-end content-end p-4">
-                <div className="mt-6 items-center">
-                  <div className="h-4"></div>
-                  <div className="cursor-pointer border-gray-400 rounded-l-xl">
-                    <LeftOutlined className="p-3 h-10" />
-                  </div>
-                </div>
-                <div className="mt-6 items-center">
-                  <div className="h-4">Check In</div>
-                  <div className=" cursor-pointer border border-gray-400">
-                    <CalendarOutlined className="p-3" />
-                    <input className="h-10 cursor-pointer rounded-r-lg" value="" />
-                  </div>
-                </div>
-                <div className="mt-6 items-center">
-                  <div className="h-4">Check Out</div>
-                  <div className="cursor-pointer  border-y  border-r border-gray-400 ">
-                    <CalendarOutlined className="p-3" />
-                    <input className="h-10 cursor-pointer rounded-r-lg" value="" />
-                  </div>
-                </div>
-                <div className="mt-6 items-center">
-                  <div className="h-4"></div>
-                  <div className="cursor-pointer border-gray-400 rounded-r-xl">
-                    <RightOutlined className="p-3 h-10" />
-                  </div>
-                </div>
-              </div>
-            </div> */}
-            <div className="mb-8 mt-8">
-              <div>{/* <DateRangePicker /> */}</div>
-              <div className="grid grid-cols-2 md:grid-cols-12  h-[400px] p-4 gap-5" key={index}>
-                <Fragment>
-                  <Link
-                    href="#"
-                    className=" border border-gray-300 rounded-md relative cursor-pointer col-span-2"
-                  >
-                    <Image
-                      className="min-h-[348px] rounded-md "
-                      src={element.resort.resortImages[0]?.link}
-                      alt=""
-                      fill
-                    />
-
-                    <div className="absolute bottom-0 text-white  w-full p-4">
-                      <div className="text-[20px] uppercase">{element.resort.resortName}</div>
-                      {/* <div className="text-common">exploration {'>'}</div> */}
-                    </div>
-                  </Link>
-                  {/* {if(pageabble.totalPages)} */}
-                  <div className="flex flex-col items-center justify-center">
-                    <div
-                      className="relative border border-gray-300 p-4 rounded-full cursor-pointer items-center"
-                      onClick={() => {
-                        var page = pageable.pageNo - 1;
-                        fetchListApartment(element.resort.id, index, page);
-                      }}
+          <div key={index} style={{ width: '100%' }}>
+            <div className="mb-4 mt-4">
+              <Fragment>
+                <Row gutter={0} justify="space-around" align="middle">
+                  <Col className="gutter-row" span={4}>
+                    <Card
+                      hoverable
+                      style={{ height: 380 }}
+                      cover={
+                        <Carousel autoplay>
+                          {element.resort.resortImages.map((image: any, index: number) => (
+                            <Image key={index} width={'auto'} height={380} src={image.link} />
+                          ))}
+                        </Carousel>
+                      }
                     >
-                      <LeftOutlined className="flex flex-row items-center justify-center" />
-                    </div>
-                  </div>
-                  {listApartment?.map((element: any, index: number) => {
-                    const startTime = new Date(element.availableTime.startTime);
-                    const endTime = new Date(element.availableTime.endTime);
-                    return (
-                      <Link
-                        key={index}
-                        className=" relative border border-gray-300 rounded-md cursor-pointer hover:-translate-y-4 hover:duration-500 translate-y-0 duration-500 col-span-2 w-[200px]"
-                        href={`/apartment/${element.availableTime.id}?propertyId=${element.coOwnerId.propertyId}&roomId=${element.coOwnerId.roomId}`}
-                      >
-                        <div>
-                          <img
-                            className="rounded-t-md h-[170px] w-[100%]"
-                            src={element.property.propertyImage[0]?.link}
-                            alt=""
-                          />
-                          <div className="px-3 py-2 ">
-                            <div>
-                              <div className="font-bold">{element.property.propertyName}</div>
-                              <div className="text-[14px] py-1">
-                                Owner:
-                                <span className="text-gray-500">{element.user.fullName}</span>
-                              </div>
-                              <div className="text-[14px]">{startTime.toDateString()}</div>
-                              <div className="text-[14px]">to</div>
-                              <div className="text-[14px]">{endTime.toDateString()}</div>
-                              <div>
-                                {element.property.rating ? (
-                                  <div className="flex flex-row items-center">
-                                    <div>
-                                      <MdStar color="orange" size={20} />
+                      <div className="absolute bottom-0 text-white pb-4" style={{ width: 'auto' }}>
+                        <div className="text-[14px] uppercase">{element.resort.resortName}</div>
+                      </div>
+                    </Card>
+                  </Col>
+                  <Col className="gutter-row">
+                    {pageable.pageNo == 0 ? (
+                      <div className="w-3"> </div>
+                    ) : (
+                      <LeftOutlined
+                        className="w-3"
+                        onClick={() => {
+                          var page = pageable.pageNo - 1;
+                          fetchListApartment(element.resort.id, index, page);
+                        }}
+                      />
+                    )}
+                  </Col>
+
+                  <Col className="gutter-row" span={18}>
+                    <Row justify="start" gutter={12}>
+                      {listApartment.map((element: any, index: number) => {
+                        const startTime = new Date(element.availableTime.startTime);
+                        const endTime = new Date(element.availableTime.endTime);
+                        return (
+                          <Col
+                            className="gutter-row"
+                            span={6}
+                            key={index}
+                            onClick={() =>
+                              handleRedirectApartmentDetail(
+                                `/apartment/${element.availableTime.id}?propertyId=${element.coOwnerId.propertyId}&roomId=${element.coOwnerId.roomId}`
+                              )
+                            }
+                          >
+                            <Card
+                              hoverable
+                              style={{ height: 380 }}
+                              cover={
+                                <Carousel>
+                                  {element.property?.propertyImage.map(
+                                    (image: any, index: number) => (
+                                      <Image
+                                        key={index}
+                                        width={'100%'}
+                                        height={200}
+                                        src={image.link}
+                                        preview={false}
+                                      />
+                                    )
+                                  )}
+                                </Carousel>
+                              }
+                            >
+                              <b> {element.property.propertyName}</b>
+                              <Meta
+                                style={{ margin: '1px' }}
+                                avatar={
+                                  element.user.avatar ? (
+                                    <Avatar src={element.user.avatar} />
+                                  ) : (
+                                    <Avatar>{element.user.username} </Avatar>
+                                  )
+                                }
+                                description={'Owner: ' + element.user.username}
+                              />
+                              <div className="px-3 py-2 ">
+                                <div>
+                                  <div className="flex">
+                                    <div className="text-[14px]">
+                                      {startTime.toLocaleDateString('en-GB')}
                                     </div>
-                                    <div className="flex flex-row items-center gap-1 mt-1">
-                                      <div className="text-[14px]">{element.property.rating}</div>
-                                      {/* <div className="text-[13px] text-gray-500">(300+)</div> */}
+                                    <div className="text-[14px] px-1"> - </div>
+                                    <div className="text-[14px]">
+                                      {endTime.toLocaleDateString('en-GB')}
                                     </div>
                                   </div>
-                                ) : (
-                                  <></>
-                                )}
-                              </div>
-                            </div>
-                            <div className="absolute bottom-0 py-3">
-                              <div className="flex flex-row items-center">
-                                <div>{element.availableTime.pricePerNight}</div>
-                                <img
-                                  className="w-[20px] h-[20px]"
-                                  src="/images/coin.png"
-                                  alt=""
-                                />{' '}
-                                <div className="text-[13px]"> /night</div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </Link>
-                    );
-                  })}
-                  <div className="flex flex-col items-center justify-center">
-                    <div
-                      className="relative border border-gray-300 rounded-full ml-10 p-4  cursor-pointer"
-                      onClick={() => {
-                        var page = pageable.pageNo + 1;
+                                </div>
+                                <div className="  bottom-0 py-3 flex justify-between">
+                                  <div className="flex flex-row items-center">
+                                    <div>{element.availableTime.pricePerNight}</div>
+                                    <img
+                                      className="w-[20px] h-[20px]"
+                                      src="/images/coin.png"
+                                      alt=""
+                                    />
+                                    <div className="text-[13px]"> /night</div>
+                                  </div>
+                                  {element.property.rating ? (
+                                    <div className="flex flex-row items-center">
+                                      <div>{element.property.rating}</div>
 
-                        fetchListApartment(element.resort.id, index, page);
-                      }}
-                    >
-                      <RightOutlined className="flex flex-row items-center justify-center" />
-                    </div>
-                  </div>
-                </Fragment>
-              </div>
-              <div className="flex justify-center">
-                {/* <Pagination
-                  currentPage={pageable.pageNo + 1}
-                  onPageChange={(page: number) => {
-                    // setCurrent(page);
-                    fetchListApartment(element.resort.id, index, page);
-                  }}
-                  showIcons
-                  totalPages={pageable.totalPages ? pageable.totalPages : 1}
-                /> */}
-              </div>
+                                      <div className="text-[13px]">
+                                        <MdStar color="orange" size={20} />
+                                      </div>
+                                    </div>
+                                  ) : (
+                                    <></>
+                                  )}
+                                </div>
+                              </div>
+                            </Card>
+                          </Col>
+                        );
+                      })}
+                    </Row>
+                  </Col>
+                  <Col className="gutter-row">
+                    {pageable.totalPages - 1 <= pageable.pageNo ? (
+                      <div className="w-3"> </div>
+                    ) : (
+                      <RightOutlined
+                        onClick={() => {
+                          var page = pageable.pageNo + 1;
+                          fetchListApartment(element.resort.id, index, page);
+                        }}
+                      />
+                    )}
+                  </Col>
+                </Row>
+              </Fragment>
             </div>
           </div>
         );
