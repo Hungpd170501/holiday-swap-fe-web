b606c6e541320bfdfb8183b5cb33bad362df5a8e
Tin Nguyen <tinitgithub@gmail.com>
2023-12-18 14:55:43 +0700

    Fix session get owner booking detail

--- app/dashboard/ownerBooking/[bookingId]/OwnerBookingDetail.tsx
+++ app/dashboard/ownerBooking/[bookingId]/OwnerBookingDetail.tsx
@@ -42,31 +42,29 @@ const OwnerBookingDetail: React.FC<OwnerBookingDetailProps> = ({
   const axiosAuthClient = useAxiosAuthClient();
   const [openModal, setOpenModal] = useState(false);
   const { data: session } = useSession();
+  const accessToken = session?.user?.access_token;
 
   const { bookingId } = ownerBookingDetailId;
 
   const handleCancelBooking = () => {
-    const accessToken = session?.user?.access_token;
-    if (accessToken) {
-      const config = { headers: { Authorization: `Bearer ${accessToken}` } };
-      axios
-        .put(`https://holiday-swap.click/api/booking/cancel/${bookingId}`, config)
-        .then(async (response) => {
-          const newDetail = await axios.get(
-            `https://holiday-swap.click/api/booking/ownerhistorybooking/${bookingId}`,
-            config
-          );
-          if (newDetail) {
-            setDetail(newDetail.data);
-          }
-          toast.success('Cancel booking successfully!');
-          setOpenModal(false);
-        })
-        .catch((response) => {
-          toast.error(response?.response?.data?.message ?? 'Something went wrong');
-          setOpenModal(false);
-        });
-    }
+    const config = { headers: { Authorization: `Bearer ${accessToken}` } };
+    axios
+      .put(`https://holiday-swap.click/api/booking/cancel/${bookingId}`)
+      .then(async () => {
+        const newDetail = await axios.get(
+          `https://holiday-swap.click/api/booking/ownerhistorybooking/${bookingId}`,
+          config
+        );
+        if (newDetail) {
+          setDetail(newDetail.data);
+        }
+        toast.success('Cancel booking successfully!');
+        setOpenModal(false);
+      })
+      .catch((response) => {
+        toast.error(response?.response?.data?.message ?? 'Something went wrong');
+        setOpenModal(false);
+      });
   };
 
   const handleContactOwner = (ownerId: string) => {
